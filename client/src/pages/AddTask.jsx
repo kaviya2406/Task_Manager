4c7398aeab7439262ff50647bac11f141fe78d9d
keerthana2k7 <keerthana.rajvanitha@gmail.com>
2026-02-08 19:18:25 +0530

    v1 basic setup fe-141

---
+++ client/src/pages/AddTask.jsx
@@ -0,0 +1,5 @@
+function AddTask() {
+  return <h2>Add Task Page</h2>;
+}
+
+export default AddTask;
---
+++ client/src/pages/EditTask.jsx
@@ -0,0 +1,5 @@
+function EditTask() {
+  return <h2>Edit Task Page</h2>;
+}
+
+export default EditTask;
